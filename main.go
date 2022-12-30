26929bbcedf1f18c6a6ca0663cfd15f355bdf879
x5iu <xuboyu72@gmail.com>
2022-12-30 10:54:22 +0800

    🐳 修复生成的代码中变量位置错乱的问题

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ var (
 var (
 	defc = &cobra.Command{
 		Use:     "defc",
-		Version: "v1.4.0",
+		Version: "v1.4.1",
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 			if genMode := modeMap[mode]; !genMode.IsValid() {
 				return fmt.Errorf("invalid mode %q, available modes are: [%s]", mode, printStrings(validModes))
