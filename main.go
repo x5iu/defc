b962da968a34eab3046532539bb163ad74910352
x5iu <xuboyu72@gmail.com>
2023-01-14 22:52:12 +0800

    🐳 更新生成代码 Header
    ^// Code generated .* DO NOT EDIT\.$
                                     ^^
                           原本的生成代码没有这个英文句号 .

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ var (
 var (
 	defc = &cobra.Command{
 		Use:           "defc",
-		Version:       "v1.9.1",
+		Version:       "v1.9.2",
 		SilenceUsage:  true,
 		SilenceErrors: true,
 		CompletionOptions: cobra.CompletionOptions{
