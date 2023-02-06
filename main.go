4f29e937e7f14cf5c33cace71db7254cc15331f0
x5iu <xuboyu72@gmail.com>
2023-02-06 16:35:44 +0800

    🚀 API 模式下，为 `Response` 新增 `SetTotalCount` 方法支持
    现在调用方可以为 `Response` 实现 `SetTotalCount(int)` 方法，这会让 `defc` 在每次分页查询结束后，将查询结果累计数量传入 `Response`，使 `Response` 可以更明确地知道何时应结束分页查询

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ var (
 var (
 	defc = &cobra.Command{
 		Use:           "defc",
-		Version:       "v1.9.6",
+		Version:       "v1.10.0",
 		SilenceUsage:  true,
 		SilenceErrors: true,
 		CompletionOptions: cobra.CompletionOptions{
