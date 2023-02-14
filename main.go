c70c662127d83c1b1815ea1973da2f2a4705d3b1
x5iu <xuboyu72@gmail.com>
2023-02-15 00:44:16 +0800

    🚀 新增 --mode=sqlx 参数 CONST
    使用 CONST 参数将把注释内容作为完整 SQL 进行数据库查询，不会进行模板构建，也无法使用模板语法

--- main.go
+++ main.go
@@ -58,7 +58,7 @@ var (
 var (
 	defc = &cobra.Command{
 		Use:           "defc",
-		Version:       "v1.11.8",
+		Version:       "v1.12.0",
 		SilenceUsage:  true,
 		SilenceErrors: true,
 		CompletionOptions: cobra.CompletionOptions{
