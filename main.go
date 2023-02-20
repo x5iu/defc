a6163322b301faf50945449c81968ea2b0795644
x5iu <xuboyu72@gmail.com>
2023-02-20 11:20:37 +0800

    🚀 完善 many 模式下的 scan 功能，更加适配分页查询场景

--- main.go
+++ main.go
@@ -58,7 +58,7 @@ var (
 var (
 	defc = &cobra.Command{
 		Use:           "defc",
-		Version:       "v1.13.0",
+		Version:       "v1.13.1",
 		SilenceUsage:  true,
 		SilenceErrors: true,
 		CompletionOptions: cobra.CompletionOptions{
